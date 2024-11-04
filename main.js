e58b42164eb1e59fafe20a9f09894d43514621fc
Uriel-Salguero <urysalguero09@gmail.com>
2024-11-04 19:18:08 -0300

    casi terminado

---
+++ main.js
@@ -0,0 +1,19 @@
+const productos = [
+    {
+        id: 1, nombre: "Café Espresso", precio: 950, descripcion: "Café intenso y aromático, preparado con granos de calidad. Ideal para un impulso rápido."},
+
+    {   
+        id: 2, nombre: "Café con leche", precio: 1050, descripcion: "Un clásico irresistible, mezcla de café premium y leche espumada."},
+    {   
+        id: 3, nombre: "Cappuccino", precio: 1100, descripcion: "Un delicioso cappuccino elaborado con un espresso de alta calidad, leche vaporizada y una generosa capa de espuma de leche. Aromatizado con un toque de canela o chocolate en polvo."},
+    {
+        id: 4, nombre: "Latte de Caramelo Cremoso", precio: 1250, descripcion: "Doble shot de espresso con leche cremosa. Perfecta armonía para el gusto exigente."},
+    {
+        id: 5, nombre: "Flat White Premium ", precio: 1250, descripcion: "Café suave con un toque dulce de caramelo y leche espumosa que acaricia el paladar."},
+    {
+        id: 6, nombre: "Mocha de Chocolate Amargo", precio: 1350, descripcion: "Café y cacao oscuro se unen en una bebida indulgente y sofisticada."},
+    {
+        id: 7, nombre: "Affogato de Vainilla ", precio: 1375, descripcion: "Helado de vainilla sumergido en espresso caliente, un postre y café en uno."},
+    {
+        id: 8, nombre: "Cold Brew Exótico", precio: 1280, descripcion: "Café frío, infusionado lentamente para un sabor refrescante y único."},
+]
